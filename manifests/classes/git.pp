51501094f8f1909cc3743c7691b9bcd11d4750b0
Jesse Newland <jnewland@gmail.com>
2008-11-22 15:58:31 -0500

    more git cleanup

--- manifests/classes/git.pp
+++ manifests/classes/git.pp
@@ -13,27 +13,30 @@ class git {
 
   exec { "download-git-tgz":
       cwd       => "/usr/local/src",
-      command   => "/usr/binwget http://kernel.org/pub/software/scm/git/git-$version.tar.gz",
+      path      => "/bin:/usr/bin",
+      command   => "wget http://kernel.org/pub/software/scm/git/git-$version.tar.gz",
       creates   => "/usr/local/src/git-$version.tar.gz",
-      unless    => "/usr/bin/git --version | grep '$version'",
+      unless    => "git --version | grep '$version'",
       before    => Exec["untar-git-source"],
       require   => [Package[wget]]
   }
 
   exec { "untar-git-source":
-      command   => "/bin/tar xzf git-$version.tar.gz",
+      command   => "tar xzf git-$version.tar.gz",
+      path      => "/bin:/usr/bin",
       cwd       => "/usr/local/src",
       creates   => "/usr/local/src/git-$version",
-      unless    => "/usr/bin/git --version | grep '$version'",
+      unless    => "git --version | grep '$version'",
       subscribe => Exec["download-git-tgz"],
       before    => Exec["make-install-git"]
   }
 
   exec { "make-install-git":
       cwd       => "/usr/local/src/git-$version",
-      command   => "/usr/bin/make prefix=/usr all && make prefix=/usr install",
+      path      => "/bin:/usr/bin",
+      command   => "make prefix=/usr all && make prefix=/usr install",
       creates   => [ "/usr/bin/git" ],
-      unless    => "/usr/bin/git --version | grep '$version'",
+      unless    => "git --version | grep '$version'",
       subscribe => Exec["untar-git-source"],
       require   => [Exec["untar-git-source"],Package[gcc],Package[make],Package[gettext-devel],Package[expat-devel],Package[curl-devel],Package[openssl-devel],Package[zlib-devel]]
   }
